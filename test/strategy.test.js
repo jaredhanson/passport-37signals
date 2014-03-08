6a1f08c625033f6aef529465b4e903852890ded7
Jared Hanson <jaredhanson@gmail.com>
2014-03-08 15:34:20 -0800

    Add test case for extended params.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -13,4 +13,14 @@ describe('Strategy', function() {
     expect(strategy.name).to.equal('37signals');
   });
   
+  it('should add web_server to authorization params', function() {
+    var params = strategy.authorizationParams();
+    expect(params.type).to.equal('web_server');
+  });
+  
+  it('should add web_server to token params', function() {
+    var params = strategy.tokenParams();
+    expect(params.type).to.equal('web_server');
+  });
+  
 });
